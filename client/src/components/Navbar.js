aedf662db9ef29379d43164497a2c5fa42a4f146
Pragallapati Saketh <pragallapati.saketh@gmail.com>
2025-04-25 23:14:40 +0530

    updated navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Navbar() {
     const location = useLocation();
